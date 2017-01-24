7827e2a4c91d1eca5195823d74fa34f035930bd1
ubuntuarif <arif.cse28@gmail.com>
2017-01-25 01:55:11 +0600

    UpdateView and Deleteview added.

--- music/urls.py
+++ music/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     # adding an album
     url(r'album/add/$', views.AlbumCreate.as_view(), name="album-add"),
 
-    # /music/album_id/favourite
-    # url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
+    # /music/album_id/update
+    url(r'album/(?P<pk>[0-9]+)/update/$', views.AlbumUpdate.as_view(), name="album-update"),
+
+    # /music/album_id/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name="album-delete"),
 ]
