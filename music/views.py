28ec5f6cbeac0bc3fcd6907b6217737c8e8ffe02
ubuntuarif <arif.cse28@gmail.com>
2017-01-24 23:38:54 +0600

    Model View added

--- music/views.py
+++ music/views.py
@@ -1,40 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from music.models import Album
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {'all_albums' : all_albums}
-    return render(request, 'music/index.html', context)
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, id=album_id)
-    return render(
-        request,
-        'music/detail.html',
-        {'album': album}
-    )
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def favourite(request, album_id):
-    album = get_object_or_404(Album, id=album_id)
-    try:
-        selected_song = album.song_set.get(id=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(
-            request,
-            'music/detail.html',
-            {
-                'album': album,
-                'error_message' : "You did not selected a valid song.",
-            }
-        )
-    else:
-        # updating fav song
-        selected_song.is_favourite = True
-        selected_song.save()
+    def get_queryset(self):
+        return Album.objects.all()
 
-        return render(
-            request,
-            'music/detail.html',
-            {'album': album}
-        )
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = [
+        'artist',
+        'album_title',
+        'genre',
+        'album_logo',
+    ]
